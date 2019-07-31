77bc8dce2fb1a4e346c8b696e93e6b5c1b6574bd
FacerAin <syw5141@daum.net>
2019-07-31 19:53:14 +0900

    Add callbackExample.js

---
+++ 2.Functions/callbackExample.js
@@ -0,0 +1,12 @@
+const list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+const callbackExample = (items, callback) =>
+    setTimeout(() =>
+        callback(list.reduce((a, b) =>
+            (a + b))), 0)
+
+callbackExample(list, (result) =>
+    console.log(result)
+)
+
+console.log('first')
